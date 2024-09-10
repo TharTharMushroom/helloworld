82e655a4c8be2e7d9de88245e7dc5f8f3b851acb
Devin <159491562+TharTharMushroom@users.noreply.github.com>
2024-09-10 09:15:43 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int a = 6;
+bool b = true;
+float c = 3.4245;
+double d = 7.98264;
+char e = 68;
+string f = "abc";
+int main()
+{
+    cout << "This is an int, which is a whole number: " << a << endl;
+    cout << "This is a bool, which is a true or false: " << b << endl;
+    cout << "This is a float, which is a short decimal: " << c << endl;
+    cout << "This is a double, which is a long decimal: " << d << endl;
+    cout << "This is a char, which is a character: " << e << endl;
+    cout << "This is a string, which is multiple characters: " << f << endl;
+}
